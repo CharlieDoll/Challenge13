0692c33f9ee4c053b0b488f0cb1609f1d870d731
CharlieDoll <113851328+CharlieDoll@users.noreply.github.com>
2023-01-02 13:38:24 +0000

    new routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -17,22 +17,52 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+// find one category by its `id` value
+// be sure to include its associated Products
 
-router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+  try {
+    const items = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(items);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const items = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.status(200).json(items);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const items = await Category.destroy({
+      where: { id: req.params.id },
+    });
+    res.status(200).json(items);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
